4147ce9288a37f5fcde33ab8163645af248b8498
Brandon Long <blong032593@gmail.com>
2017-09-21 22:24:51 -0700

    testing

--- app/jobs/onebusaway.py
+++ app/jobs/onebusaway.py
@@ -6,9 +6,11 @@ from jobs import AbstractJob
 class Onebusaway(AbstractJob):
 
     def __init__(self, conf):
+        self.url = conf['url']
         self.interval = conf['interval']
+        self.timeout = conf.get('timeout')
 
     def get(self):
-        # r = requests.get(self.url, timeout=self.timeout)
-        # r.raise_for_status()
-        return "{'json':'lol}"
+        r = requests.get(self.url, timeout=self.timeout)
+        r.raise_for_status()
+        return r.json()
--- app/static/widgets/onebusaway/onebusaway.js
+++ app/static/widgets/onebusaway/onebusaway.js
@@ -1,11 +1,48 @@
 var onebusaway = onebusaway || {};
 
+onebusaway.parseState = function (data) {
+  var body = data;
+
+  body.entry.arrivalsAndDepartures.forEach(function (d) {
+    var departureTime = moment(d.predictedArrivalTime).locale('en-us'),
+        now = moment();
+    if (departureTime.isBefore(now)) {
+    } else {
+        d.departureTime = departureTime;
+    }
+  });
+
+  if (body.departures.length > 0) {
+    body.next = body.departures[0];
+    body.upcoming = {
+        'line': body.departures.routeShortName
+    }
+    // body.upcoming = body.departures.slice(1, 5);
+  } else {
+    body.next = null;
+    body.upcoming = [];
+  }
+  return body;
+};
+
 onebusaway.view = function (vnode) {
   if (Object.keys(vnode.attrs.data).length === 0) {
-    return [
-        m('p', 'Waiting for data'),
-        m('h1', 'Some H1')
-    ]
+    return m('p', 'Waiting for data');
   }
-  return m('pre', JSON.stringify(vnode.attrs.data, null, '  '));
+  var state = onebusaway.parseState(vnode.attrs.data);
+  var rows = state.upcoming.map(function (departure) {
+    return m('tr', [
+      m('td', {'class': 'destination'}, departure.line),
+      m('td.time', departure.departureTime.format('HH:mm'))
+    ]);
+  });
+  return [
+    m('p.fade', 'Bus ' + state.next.line + ' til ' +
+      state.next.destination),
+    m('h1', state.next.departureTime.format('HH:mm')),
+    m('h2', state.next.departureTime.fromNow()),
+    m('table', rows),
+    m('p', {'class': 'fade updated-at'}, 'Sist oppdatert: ' +
+      state.updatedAt)
+  ];
 };
